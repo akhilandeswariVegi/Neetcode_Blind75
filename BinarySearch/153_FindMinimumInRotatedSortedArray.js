22657a9db4849ff301118b1d9a7fcabca0c4eb6f
Akhila <akhilavegi120@gmail.com>
2024-04-03 23:34:28 -0500

    Update 153_FindMinimumInRotatedSortedArray.js

--- BinarySearch/153_FindMinimumInRotatedSortedArray.js
+++ BinarySearch/153_FindMinimumInRotatedSortedArray.js
@@ -10,3 +10,19 @@ Input: nums = [3,4,5,1,2]
 Output: 1
 Explanation: The original array was [1,2,3,4,5] rotated 3 times.
 */
+var findMin = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let mid = left + Math.floor((right - left) / 2);
+
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+
+    return nums[left];
+};
