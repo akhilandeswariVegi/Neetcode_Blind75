0b960ebb75dd0ec911b943c60c4ced194794227a
AkhiVegi <axv81730@ucmo.edu>
2024-04-09 23:20:37 -0500

    feat: "linkedlist problems"

--- LinkedList/206_ReverseLinkedList.js
+++ LinkedList/206_ReverseLinkedList.js
@@ -1,5 +1,6 @@
 /*
 Given the head of a singly linked list, reverse the list, and return the reversed list.*/
+
 var reverseList = function (head) {
     let [ prev, curr, next ] = [ null, head, null ];
 
