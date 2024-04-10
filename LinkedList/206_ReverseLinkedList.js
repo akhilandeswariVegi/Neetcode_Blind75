7d5cf70f9a86535663087227da7e487b2da4eb04
AkhiVegi <axv81730@ucmo.edu>
2024-04-09 23:15:46 -0500

    feat: "linked list"

--- LinkedList/206_ReverseLinkedList.js
+++ LinkedList/206_ReverseLinkedList.js
@@ -1,2 +1,15 @@
 /*
 Given the head of a singly linked list, reverse the list, and return the reversed list.*/
+var reverseList = function (head) {
+    let [ prev, curr, next ] = [ null, head, null ];
+
+    while (curr) {/* Time O(N) */
+        next = curr.next;
+        curr.next = prev;
+
+        prev = curr;
+        curr = next;
+    }
+
+    return prev;
+};
