bc1b96a5ac05ed217e197a56a5213de52f197fff
Akhila <akhilavegi120@gmail.com>
2024-06-10 13:47:03 -0500

    Update 33_SearchInRotatedSortedArray.js

--- BinarySearch/33_SearchInRotatedSortedArray.js
+++ BinarySearch/33_SearchInRotatedSortedArray.js
@@ -7,3 +7,43 @@ Example 1:
 Input: nums = [4,5,6,7,0,1,2], target = 0
 Output: 4
 */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        // Check if the mid element is the target
+        if (nums[mid] === target) {
+            return mid; // <--- Return when target is found
+        }
+
+        // Determine which side is sorted
+        if (nums[left] <= nums[mid]) {
+            // Left side is sorted
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1; // Target is in the left half
+            } else {
+                left = mid + 1; // target doesn't exists, so Target may be in the right half
+            }
+        } else {
+            // Right side is sorted
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1; // Target is in the right half
+            } else {
+                right = mid - 1; // target doesn't exists, , so Target may be in the left half
+            }
+        }
+    }
+
+    // Target was not found
+    return -1; // <--- Return when target is not found
+};
+
