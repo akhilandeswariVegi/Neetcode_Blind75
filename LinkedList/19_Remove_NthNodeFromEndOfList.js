a9f15c6c027aa63f7b71c1bc9bdffb7cc3a0cf81
Akhila <akhilavegi120@gmail.com>
2024-06-12 14:01:33 -0500

    Update 19_Remove_NthNodeFromEndOfList.js

--- LinkedList/19_Remove_NthNodeFromEndOfList.js
+++ LinkedList/19_Remove_NthNodeFromEndOfList.js
@@ -1,3 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
 /*
-Given the head of a linked list, remove the nth node from the end of the list and return its head.
+Input: head = [1,2,3,4,5], n = 2
+Output: [1,2,3,5]
 */
+var removeNthFromEnd = function (head, n) {
+
+    let dummy = new ListNode();
+    dummy.next = head;
+    let fast = dummy;
+    let slow = dummy;
+
+    // Move fast pointer n steps ahead
+    for (let i = 0; i <= n; i++) {
+        fast = fast.next;
+    }
+
+    // Move both fast and slow pointers until fast reaches the end
+    while (fast !== null) {
+        fast = fast.next;
+        slow = slow.next;
+    }
+
+    // Remove the nth node from end
+    slow.next = slow.next.next;
+
+    return dummy.next;
+
+};
